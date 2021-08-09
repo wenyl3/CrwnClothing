774f8796a8abd01ddbeb19ae3c3d42d993562091
wenyl3 <wenyl3@github.com>
2021-08-09 14:23:16 +0300

    Added sign-in component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInANdSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInANdSignUpPage} />
         </Switch>
         {/* <HomePage /> */}
       </div>
