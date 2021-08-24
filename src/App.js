4ecdcc6224e71ab6ae14fb72cbe02b2f008caf91
wenyl3 <wenyl3@github.com>
2021-08-24 21:41:22 +0300

    Minor changes; fix typo, add property to google sign-in button

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInANdSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
@@ -51,9 +51,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInANdSignUpPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
-        {/* <HomePage /> */}
       </div>
     );
   }
