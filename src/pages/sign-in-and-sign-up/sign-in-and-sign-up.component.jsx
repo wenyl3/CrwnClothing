dbd989180150e679447af656acc745a4eb0c64d8
wenyl3 <wenyl3@github.com>
2021-08-25 22:27:08 +0300

    Setup redux

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -3,11 +3,11 @@ import './sign-in-and-sign-up.styles.scss';
 import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 
-const SignInANdSignUpPage = () => (
+const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
         <SignUp />
     </div>
 );
 
-export default SignInANdSignUpPage;
+export default SignInAndSignUpPage;
