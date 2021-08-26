07372f2ad69261c963f5f05cc213cc94a517845b
wenyl3 <wenyl3@github.com>
2021-08-26 22:11:08 +0300

    Added cart drop-down; implemented cart reducer

---
+++ src/redux/cart/cart.reducer.js
@@ -0,0 +1,19 @@
+import CartActionTypes from './cart.types';
+
+const INITIAL_STATE = {
+    hiddenValue: true
+};
+
+const cartReducer = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+        return {
+            ...state,
+            hiddenValue: !state.hiddenValue
+        };
+    default:
+        return state;
+    }
+};
+
+export default cartReducer;
