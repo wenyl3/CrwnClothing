40d5ef30ed9abb64617d8e09044a5584d9ca2a0e
wenyl3 <wenyl3@github.com>
2021-08-29 00:57:09 +0300

    Added functionality to add and remove item at checkout using arrows

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from './cart.utils';
 
 const INITIAL_STATE = {
     hiddenValue: true,
@@ -24,8 +24,14 @@ const cartReducer = (state = INITIAL_STATE, action) => {
             return {
                 ...state,
                 cartItems: state.cartItems.filter(
-                    cartItem => cartItem.id != action.payload.id)
+                    cartItem => cartItem.id !== action.payload.id)
             };
+        }
+        case CartActionTypes.REMOVE_ITEM: {
+            return {
+                ...state,
+                cartItems: removeItemFromCart(state.cartItems, action.payload)
+            }
         }
         default:
             return state;
