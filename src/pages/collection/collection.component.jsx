f99cac96370c7745da1b79af861f146223595bce
wenyl3 <wenyl3@github.com>
2021-09-02 22:04:21 +0300

    Added collection routing (not finished)

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => (
+    <div className='collection-page'>
+        <h1>COLLECTION PAGE</h1>
+    </div>
+);
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+})
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,16 @@
 import React from 'react';
-import SHOP_DATA from './shop.data';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+import { Route } from 'react-router-dom';
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+import CollectionPage from '../collection/collection.component';
 
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherColelctionProps }) => (
-                        <CollectionPreview key={id} {...otherColelctionProps} />
-                    ))
-                }
-            </div>);
-    }
-}
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
+
+const ShopPage = ({ match }) => (
+    < div className='shop-page' >
+        <Route exact path={`${match.path}`} component={CollectionsOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div >
+);
 
 export default ShopPage;
