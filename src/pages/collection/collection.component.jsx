484896ffece7961e122c346f61d2f83544d1cf60
wenyl3 <wenyl3@github.com>
2022-01-04 10:59:56 +0200

    Created selector and updated respective components; changed data type from list to object

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -7,11 +7,20 @@ import CollectionItem from '../../components/collection-item/collection-item.com
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => (
-    <div className='collection-page'>
-        <h1>COLLECTION PAGE</h1>
-    </div>
-);
+const CollectionPage = ({ collection }) => {
+    const { title, items } = collection;
+
+    return (
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+                {
+                    items.map(item => <CollectionItem key={item.id} item={item} />)
+                }
+            </div>
+        </div>
+    );
+};
 
 const mapStateToProps = (state, ownProps) => ({
     collection: selectCollection(ownProps.match.params.collectionId)(state)
